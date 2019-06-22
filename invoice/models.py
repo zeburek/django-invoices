80d041a7aa5a4518bb64bc675457adabb35043d3
Parviz Khavari <csctclan@gmail.com>
2019-06-22 19:55:07 +0300

    Add russian translations for edit forms and admin panel

--- invoice/models.py
+++ invoice/models.py
@@ -1,28 +1,37 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.IntegerField()
+    name = models.CharField(max_length=200, verbose_name=_("product_name"))
+    price = models.IntegerField(verbose_name=_("product_price"))
+
+    class Meta:
+        verbose_name = _('product_model')
+        verbose_name_plural = _('products_model')
 
     def __str__(self):
         return f"{self.name}, цена: {self.price}"
 
 
 class Client(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, verbose_name=_("client_name"))
+
+    class Meta:
+        verbose_name = _('client_model')
+        verbose_name_plural = _('clients_model')
 
     def __str__(self):
         return f"{self.name}"
 
 
 class Invoice(models.Model):
-    number = models.IntegerField()
-    date = models.DateField()
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    number = models.IntegerField(verbose_name=_("invoice_number"))
+    date = models.DateField(verbose_name=_("invoice_date"))
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name=_("invoice_client"))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("invoice_created_at"))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("invoice_updated_at"))
 
     def __str__(self):
         return f"{self.number} от {self.date} для {self.client}"
@@ -32,12 +41,12 @@ class Invoice(models.Model):
 
 
 class Released(models.Model):
-    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    qty = models.IntegerField()
-    discount = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE, verbose_name=_("released_invoice"))
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name=_("released_product"))
+    qty = models.IntegerField(verbose_name=_("released_qty"))
+    discount = models.IntegerField(default=0, verbose_name=_("released_discount"))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("released_created_at"))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("released_updated_at"))
 
     @property
     def summary(self):
--- invoices/settings.py
+++ invoices/settings.py
@@ -116,6 +116,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = [BASE_DIR + "/locale"]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
